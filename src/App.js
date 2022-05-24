0faeff88b9382d9870d8aab239b8358185d5167e
Stefano Casasola <ste.casasola@gmail.com>
2022-05-24 10:46:50 +0200

    Adds the assets and starts the Home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css'
 // importing the bootstrap css file is required!
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import CustomNavbar from './components/CustomNavbar'
+
 // the curly brackets are necessary for importing a component that has been
 // exported NOT as default
 // you DON'T need the curly brackets if you're importing something that HAS
@@ -9,33 +10,10 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 
 const App = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Dank memes
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <CustomNavbar title="Strivestaurant" />
+      <div>CONTENT</div>
+    </>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,2 @@
+// the Home component is going to have the carousel
+// and the comments section!
