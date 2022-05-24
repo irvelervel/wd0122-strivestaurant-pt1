31204e2ceabe1f840ae47ebb0affd9299f9ca63b
Stefano Casasola <ste.casasola@gmail.com>
2022-05-24 12:00:43 +0200

    Creates the carousel slides dinamically

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 // importing the bootstrap css file is required!
 import 'bootstrap/dist/css/bootstrap.min.css'
 import CustomNavbar from './components/CustomNavbar'
+import Home from './components/Home'
 
 // the curly brackets are necessary for importing a component that has been
 // exported NOT as default
@@ -12,7 +13,7 @@ const App = () => {
   return (
     <>
       <CustomNavbar title="Strivestaurant" />
-      <div>CONTENT</div>
+      <Home />
     </>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,2 +1,41 @@
 // the Home component is going to have the carousel
 // and the comments section!
+
+import { Component } from 'react'
+import { Carousel, Container, Row, Col } from 'react-bootstrap'
+import arrayOfPastas from '../data/menu.json'
+
+// let's create Home!
+class Home extends Component {
+  // the render() method is the ONLY MANDATORY part
+  // in a class component
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-center mt-3">
+          {/* <Col className="col col-xs-12 col-md-6"> */}
+          <Col xs={12} md={6}>
+            {/* curly brackets are in place because the value of the prop is a number! */}
+            <Carousel>
+              {arrayOfPastas.map((pasta) => (
+                <Carousel.Item key={pasta.id}>
+                  <img
+                    className="d-block w-100"
+                    src={pasta.image}
+                    alt="First slide"
+                  />
+                  <Carousel.Caption>
+                    <h3>{pasta.name}</h3>
+                    <p>{pasta.description}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default Home
